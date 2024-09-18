4638563694d9584cb592c4d34125f2a93909fbc6
minjeoong <orange_mj@naver.com>
2024-09-18 20:44:19 +0900

    ✨feat: 이미지 추가

--- src/styles/images.ts
+++ src/styles/images.ts
@@ -47,6 +47,30 @@ import together_img from '../../public/assets/image/img-together.svg';
 import vaccine_img from '../../public/assets/image/img-info-vaccine.svg';
 
 
+// 팀원 이미지
+import img_heabin from "../../public/assets/img/변해빈.png";
+import img_minjeong from "../../public/assets/img/김민정.png";
+import img_hyungju from "../../public/assets/img/성형주.png";
+import img_yena from "../../public/assets/img/전예나.png";
+import img_sohyun from "../../public/assets/img/오소현.png";
+import img_jieun from "../../public/assets/img/김지은.png";
+
+// 벡곰 이미지
+import img_introVecgom from "../../public/assets/img/introVecgom.svg";
+import img_vecgomFace from "../../public/assets/img/img_vecgomFace.svg";
+
+// etc
+import img_Accessibility from "../../public/assets/img/img_accessibility.svg";
+import img_innovation from "../../public/assets/img/img_innovation.svg";
+import img_Sustainability from "../../public/assets/img/img_Sustainability.svg";
+import img_SocialImpact from "../../public/assets/img/img_SocialImpact.svg";
+
+// 아이콘
+import icon_mail from "../../public/assets/icon/icon_mail.svg";
+import icon_blog from "../../public/assets/icon/icon_blog.svg";
+import icon_github from "../../public/assets/icon/icon_git.svg";
+import icon_linkedin from '../../public/assets/icon/icon_linkedin.svg';
+
 // prettier-ignore
 export const Images: ImagesType = {
   // icon
@@ -64,6 +88,20 @@ export const Images: ImagesType = {
   radio_unselec_dis : radio_unselec_dis,
   radio_unselec_en :radio_unselec_en,
   small_logo:small_logo,
+  
+  
+  icon_mail: icon_mail,
+  icon_linkedin: icon_linkedin,
+  icon_blog: icon_blog,
+  icon_github: icon_github,
+  
+  
+  
+  
+  
+  
+  
+  
   //image
   vacgom01: Vacgom01,
   vacgom : vacgom,
@@ -94,6 +132,26 @@ export const Images: ImagesType = {
   sohyun_4:sohyun_4,
   sohyun_5:sohyun_5,
   haebin_1:haebin_1,
-
-
+  
+  
+  // 팀원 이미지
+  img_heabin: img_heabin,
+  img_minjeong: img_minjeong,
+  img_hyungju: img_hyungju,
+  img_yena: img_yena,
+  img_sohyun: img_sohyun,
+  img_jieun: img_jieun,
+  
+  // 백곰 캐릭터 관련 이미지
+  img_introVecgom: img_introVecgom,
+  img_vecgomFace: img_vecgomFace,
+  
+  // etc
+  img_Accessibility : img_Accessibility,
+  img_innovation : img_innovation,
+  img_Sustainability : img_Sustainability,
+  img_SocialImpact : img_SocialImpact,
+  
+  
+  
 };
