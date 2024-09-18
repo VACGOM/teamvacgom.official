9ba9a0f4f3ca273d4110b9fa8425914d48240f86
minjeoong <orange_mj@naver.com>
2024-09-18 20:44:32 +0900

    ✨feat: 팀카드 컴포넌트

---
+++ src/app/_component/molecule/TeamCard/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { TeamCardWrap } from "./style";
+import Image from "next/image";
+import { Images } from "@/styles/images";
+
+type props = {
+  img: string;
+  name: string;
+  part?: { main: string; sub?: string };
+  link?: { mail: string; linkedIn: string; github: string; blog: string };
+};
+
+const TeamCard: React.FC<props> = ({
+  img = Images.img_heabin,
+  name = "이름",
+  part = { main: "메인 파트", sub: "서브파트" },
+  link = { mail: "", linkedIn: "", github: "", blog: "" },
+}: props) => {
+  return (
+    <TeamCardWrap>
+      <div className="top">
+        <Image src={img} alt={"팀원 이미지"} />
+      </div>
+      <div className="bottom">
+        <div className="intro">
+          <div className="name">{name}</div>
+          <div className="part">
+            <div className="main">{part.main}</div>
+            {part.sub && <div className="sub">・</div>}
+            <div className="sub">{part.sub}</div>
+          </div>
+        </div>
+        <div className="linkWrap">
+          <a href={link.mail} className="link">
+            <Image src={Images.icon_mail} alt={"메일"} />
+          </a>
+          <a href={link.linkedIn} className="link">
+            <Image src={Images.icon_linkedin} alt={"링크드인"} />
+          </a>
+          <a href={link.github} className="link">
+            <Image src={Images.icon_github} alt={"깃허브"} />
+          </a>
+          <a href={link.blog} className="link">
+            <Image src={Images.icon_blog} alt={"블로그"} />
+          </a>
+        </div>
+      </div>
+    </TeamCardWrap>
+  );
+};
+
+export default TeamCard;
---
+++ src/app/_component/molecule/TeamCard/style.ts
@@ -0,0 +1,65 @@
+import styled from "@emotion/styled";
+
+export const TeamCardWrap = styled.div`
+  width: 317px;
+  height: 500px;
+
+  border-radius: 20px;
+  border: 1px solid #e5e8eb;
+  box-sizing: border-box;
+
+  & > .top {
+    overflow: hidden;
+    & > img {
+      display: block; // 이미 주졉ㄴ 여백 제거
+      border-radius: 20px 20px 0 0;
+      overflow: hidden;
+    }
+  }
+  & > .bottom {
+    padding: 38px 30px 40px 30px;
+    & > .intro {
+      display: flex;
+      flex-direction: row;
+      gap: 10px;
+      & > .name {
+        color: #191f28;
+        text-align: center;
+        font-family: Pretendard;
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 32px;
+      }
+      & > .part {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 5px;
+        color: #4196fd;
+        text-align: center;
+        font-family: Pretendard;
+        font-size: 18px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 26px;
+      }
+    }
+    & > .linkWrap {
+      display: flex;
+      //gap: auto;
+      justify-content: space-between;
+      margin-top: 14px;
+      & > .link {
+        display: flex;
+        width: 56px;
+        height: 56px;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        border-radius: 60px;
+        border: 1px solid var(--Gray-Gray-200, #e5e8eb);
+      }
+    }
+  }
+`;
