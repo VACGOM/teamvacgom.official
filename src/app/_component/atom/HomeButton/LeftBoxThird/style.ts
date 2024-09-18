044bcf75b51394cbb45fbafa522a1c4c55cb2f9b
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-09-18 21:17:09 +0900

    🐛 fix :: 불필요한 코드 삭제

--- src/app/_component/atom/HomeButton/LeftBoxThird/style.ts
+++ src/app/_component/atom/HomeButton/LeftBoxThird/style.ts
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-export const LeftBoxContainer= styled.div`
+export const LeftBoxContainer = styled.div`
   display: flex;
   width: 100%;
   min-width: 590px;
@@ -10,32 +10,27 @@ export const LeftBoxContainer= styled.div`
   align-items: flex-start;
   gap: 20px;
   border-radius: 20px;
-background: var(--Gray-Gray-100, #F2F4F6);
+  background: var(--Gray-Gray-100, #f2f4f6);
 
-  background-image: url("/assets/image/ico-home-left-on.svg");
+  background-image: url('/assets/image/ico-home-left-on.svg');
   background-size: contain;
   background-position: 140px 0px;
   background-repeat: no-repeat;
-  cursor:pointer;
-`
+  cursor: pointer;
+`;
 
-export const IntroTitle= styled.div`
-color: var(--Gray-Gray-700, #4E5968);
-font-family: Pretendard;
-font-size: 24px;
-font-style: normal;
-font-weight: 400;
+export const IntroTitle = styled.div`
+  color: var(--Gray-Gray-700, #4e5968);
+  font-family: Pretendard;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+`;
 
-`
-
-
-export const VacgomText= styled.div`
-color: var(--Gray-Gray-700, #4E5968);
-font-family: Montserrat;
-font-size: 56px;
-font-style: normal;
-font-weight: 600;
-line-height: 110%; /* 61.6px */
-letter-spacing: -1.68px;
-
-`
+export const VacgomText = styled.div`
+  color: var(--Gray-Gray-700, #4e5968);
+  font-family: Montserrat;
+  font-size: 56px;
+  font-style: normal;
+  font-weight: 600;
+`;
