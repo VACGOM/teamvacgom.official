9040fef0785a144b5eb4e6738789b886a023cd63
osohyun0224 <53892427+osohyun0224@users.noreply.github.com>
2024-09-18 21:43:05 +0900

    🐛 fix :: 마지막으로 타입 에러 수정

--- src/app/_component/molecule/moleculeType.ts
+++
@@ -1,8 +0,0 @@
-import { SerializedStyles } from '@/app/_component/atom/atomType';
-import React from 'react';
-
-export interface MainHeaderType {
-  title: string;
-  url?: string;
-  onClick?: React.MouseEventHandler<HTMLSpanElement>;
-}
